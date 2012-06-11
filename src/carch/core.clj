c0a0da9c56bb5353f7c5cf0b2c71a562c48f131c
Jukka Villstedt <jvillste@hotmail.com>
2012-06-11 23:00:22 +0300

    x

--- src/carch/core.clj
+++ src/carch/core.clj
@@ -1,4 +1,32 @@
 (ns carch.core
-  (:import [JavaPhotoArchive Archiver]))
+  (:import [JavaPhotoArchive Archiver]
+           [java.io File]))
 
-(Archiver/main (into-array String []))
+(comment (Archiver/main (into-array String [])))
+
+(defn operating-system []
+  (case (System/getProperty "os.name")
+    "Linux" :linux
+    "Windows" :windows
+    :unknown))
+
+(defn roots []
+  (case (operating-system)
+    :linux (-> (File. "/media")
+               (.listFiles))
+    :windows (File/listRoots)))
+
+(defn source-directory []
+  (first (filter #(-> (File. (str (.getPath %) "/DCIM"))
+                      (.exists))
+                 (roots))))
+
+(defn find-files [directory extension]
+  (reduce (fn [files file] (if (.isDirectory file)
+                             (concat files (find-files file extension))
+                             (if (.endsWith (.toLowerCase (.getName file)) (.toLowerCase extension))
+                               (conj files file)
+                               files)))
+          []
+          (.listFiles directory)))
+
